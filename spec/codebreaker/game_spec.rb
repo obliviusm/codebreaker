1aba9e99347b8fdaacd34e56056338c3c9a0a8b9
obliviusm <obliviusm@gmail.com>
2013-10-17 20:11:08 +0300

    Describing Code with RSpec

---
+++ spec/codebreaker/game_spec.rb
@@ -0,0 +1,12 @@
+# file spec/codebreaker/game_spec.rb
+ 
+require_relative '../spec_helper'
+ 
+module Codebreaker
+  describe Game do
+    describe "#start" do
+      it "sends a welcome message"
+      it "prompts for the first guess"
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+# file spec/spec_helper.rb
+ 
+require 'bundler/setup'
+require 'codebreaker'
