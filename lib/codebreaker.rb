aac973bde06e1f98afebda041d046e559afd1dc9
obliviusm <obliviusm@gmail.com>
2013-10-17 23:03:21 +0300

    play with console

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,16 +1,32 @@
 # file lib/codebreaker.rb
-require "codebreaker/version"
-require 'codebreaker/game'
+require_relative "codebreaker/version"
+require_relative 'codebreaker/game'
 
 module Codebreaker
   class Game
+  	attr_accessor :code
+
     def initialize(output)
       @output = output
     end
  
     def start
-      @output.puts 'Welcome to Codebreaker!'
-      @output.puts 'Enter guess:'
+    	generate
+      	@output.puts 'Welcome to Codebreaker!'
+      	@output.puts 'Enter guess:'
+    end
+
+    def enter input
+    	@input = input
+    end
+
+    def mark 
+    	@output.puts @input
     end
   end
 end
+
+my_game = Codebreaker::Game.new (IO.new STDOUT.fileno)
+my_game.start
+my_game.enter gets
+my_game.mark
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -4,10 +4,10 @@ require_relative '../spec_helper'
  
 module Codebreaker
   describe Game do
+  	let(:output) { double('output').as_null_object }
+    let(:game)   { Game.new(output) }
+
     describe "#start" do
-      let(:output) { double('output').as_null_object }
-      let(:game)   { Game.new(output) }
- 
       it "sends a welcome message" do
         output.should_receive(:puts).with('Welcome to Codebreaker!')
         game.start
@@ -17,6 +17,20 @@ module Codebreaker
         output.should_receive(:puts).with('Enter guess:')
         game.start
       end
+    end
+
+    describe "#generate_code" do
+    	let(:code) { game.generate_code }
+    	it "generates code" do
+    		game
+    	end
+    end
+
+    describe "#mark" do
+    	it "answer with ++++ if inputted_code == generated_code" do 
+    		generated_code = "1111"
+    		game.mark("1111").should == "++++" 
+    	end
     end
   end
 end
